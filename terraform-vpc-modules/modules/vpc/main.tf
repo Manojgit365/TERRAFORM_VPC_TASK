0c4e02805a38f5f915640a5ecb506e1a3e70e4c7
Manojgit365 <hellboymanoj877@gmail.com>
2025-05-08 15:13:41 +0530

    Create main.tf

---
+++ terraform-vpc-modules/modules/vpc/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+}
